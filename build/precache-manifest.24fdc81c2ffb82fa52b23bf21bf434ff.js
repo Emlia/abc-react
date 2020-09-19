727927d0b4d6fca9b0ede8d625698cd81f368ac6
emlia <771631347@qq.com>
2020-09-19 12:04:13 +0800

    first build

---
+++ build/precache-manifest.24fdc81c2ffb82fa52b23bf21bf434ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56b1097ce9b2a2da42f881235f5fa080",
+    "url": "./index.html"
+  },
+  {
+    "revision": "97361b3331dc244e057e",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7f1829cd49e0f23d834d",
+    "url": "./static/js/2.ac6367c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ac6367c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97361b3331dc244e057e",
+    "url": "./static/js/main.6a0164ef.chunk.js"
+  },
+  {
+    "revision": "9f89922f23e65d33868b",
+    "url": "./static/js/runtime-main.591448dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
